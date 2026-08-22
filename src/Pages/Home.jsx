8fba51a0777974fc8080bf0c7d5a1b50c55ce648
Deepak <deepakbhankhor758@gmail.com>
2026-08-22 15:47:37 +0530

    remove pricing section in the home pages

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,6 @@ import Home1 from "../compnent/Home/Home1";
 import Home2 from "../compnent/Home/Home2";
 import Home3 from "../compnent/Home/Home3";
 import Home4 from "../compnent/Home/Home4";
-import Home5 from "../compnent/Home/Home5";
 import Home6 from "../compnent/Home/Home6";
 
 function Home() {
@@ -12,7 +11,6 @@ function Home() {
       <Home2 />
       <Home3 />
       <Home4 />
-      <Home5 />
       <Home6/>
     </div>
   );
