30b5557623c028bd735069d6250c6368e09479dc
Deepak <deepakbhankhor758@gmail.com>
2026-09-03 13:19:49 +0530

    remove file in service and work section for review

--- src/Pages/Service.jsx
+++ src/Pages/Service.jsx
@@ -1,6 +1,6 @@
 import Service1 from "../compnent/Service/Service1";
 import Service2 from "../compnent/Service/Service2";
-import Service3 from "../compnent/Service/Service3";
+
 import Service4 from "../compnent/Service/Service4";
 import Service5 from "../compnent/Service/Service5";
 
@@ -11,7 +11,6 @@ function Service() {
       <Service2 />
       <Service4/>
       <Service5/>
-      <Service3/>
       
     </>
   );
--- src/Pages/Work.jsx
+++ src/Pages/Work.jsx
@@ -1,6 +1,5 @@
 import Work1 from "../compnent/Work/Work1";
 import Work2 from "../compnent/Work/Work2";
-import Work3 from "../compnent/Work/Work3";
 import Work4 from "../compnent/Work/Work4";
 import Work5 from "../compnent/Work/Work5";
 import DestinationsGrid from "../compnent/Work/DestinationsGrid";
@@ -12,7 +11,7 @@ export default function Work() {
       <Work2 />
       <Work5 />
       <DestinationsGrid />
-      <Work3 />
+    
       <Work4 />
     </>
   );
